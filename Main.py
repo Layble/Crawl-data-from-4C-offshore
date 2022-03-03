864cbbe1facf543d672ca0292d574295957f6e61
Layble <51980454+Layble@users.noreply.github.com>
2022-03-03 17:05:19 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,10 +5,10 @@ selenium+chromedriver
 1、Check chrome and chromedrive version
 2、Chrome --- macos: /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
                           windows: chrome.exe --remote-debugging-port=
-3、Extract URL from 《hqq_detail_urlsURL》 python hqq_spider.py
-4、hqq_detail_urls by second_extract_html()
-5、《404_url.txt》 and 《detail_err.txt》 have some errors, could be manually collected
-7、《hqq_result.json》 first 500 items without url
+3、Extract URL from 《Detail_urlsURL》 python hqq_spider.py
+4、Detail_urls by second_extract_html()
+5、《404_url.txt》 and 《detail_err.txt》 have some errors, have to be manually collected
+7、《Result.json》 first 500 items have no urls
 '''
 
 
@@ -70,7 +70,7 @@ def extract_detail(html, result, url):
     result['distance_from_shore'] = re_search(html,
                                     r'<div id="Body_Main_Content_developerDistanceLabel" class="col-6">10.500 km</div>')
     json_str = json.dumps(result, ensure_ascii=False)
-    with open('hqq_spider/hqq_result.json', 'a+') as f:
+    with open('Code/Result.json', 'a+') as f:
         f.write(json_str)
         f.write('\n')
     print(json_str)
@@ -82,7 +82,7 @@ def second_extract(html, first_url):
     for url in urls:
         result_urls.append(first_url + url)
         print('SECOND: ', first_url + url)
-        with open('hqq_spider/hqq_detail_urls', 'a+') as f:
+        with open('Code/Detail_urls', 'a+') as f:
             f.write(first_url + url)
             f.write('\n')
     return result_urls
@@ -90,9 +90,9 @@ def second_extract(html, first_url):
 
 def second_extract_html():
     url_head = 'https://www.4coffshore.com/windfarms/'
-    file_list = os.listdir('hqq_spider/list_html')
+    file_list = os.listdir('Code/list_html')
     for file_name in file_list:
-        f = open('hqq_spider/list_html/' + file_name)
+        f = open('Code/list_html/' + file_name)
         html = f.read()
         f.close()
         url = url_head + file_name.replace('.html', '') + '/'
@@ -100,7 +100,7 @@ def second_extract_html():
 
 
 def detail_extract_start():
-    with open('hqq_spider/hqq_detail_urls', 'r') as f:
+    with open('Code/Detail_urls', 'r') as f:
         for line in f:
             url = line.replace('\n', '')
             print('DOWN URL: %s' % url)
@@ -112,14 +112,14 @@ def detail_extract_start():
                     if i == 1:
                         break
                     if '404 - Page Not Found' in html:
-                        # 404页面找不到情况
-                        print('404错误页面： %s' % url)
-                        with open('hqq_spider/404_url.txt', 'a+') as f:
+                        # 404 page cannot be found
+                        print('404 Page Not Found： %s' % url)
+                        with open('Code/404_url.txt', 'a+') as f:
                             f.write(url)
                             f.write('\n')
                     else:
-                        # 验证码或者调整到list页面的情况
-                        with open('hqq_spider/detail_err.txt', 'a+') as f:
+                        # if verification code
+                        with open('Code/detail_err.txt', 'a+') as f:
                             f.write(url)
                             f.write('\n')
                         time.sleep(60)
